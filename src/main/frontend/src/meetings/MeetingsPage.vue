68b7615108586720c9b38c39bcc87ee0b224733c
Panda <ksek@student.agh.edu.pl>
2023-05-09 17:28:29 +0200

    fullstack done

--- src/main/frontend/src/meetings/MeetingsPage.vue
+++ src/main/frontend/src/meetings/MeetingsPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <NewMeetingForm @added="addNewMeeting($event)"></NewMeetingForm>
+      <div :class="'alert alert-' + (this.isError ? 'error' : 'success')" v-if="message">{{ message }}</div>
     <span v-if="meetings.length == 0">Brak zaplanowanych spotkań.</span>
     <h3 v-else>Zaplanowane zajęcia ({{ meetings.length }})</h3>
     <MeetingsList :meetings="meetings"
@@ -14,28 +15,104 @@
 <script>
 import NewMeetingForm from "./NewMeetingForm";
 import MeetingsList from "./MeetingsList";
+import axios from "axios";
 
 export default {
   components: {NewMeetingForm, MeetingsList},
   props: {username: String},
   data() {
-    return {
-      meetings: []
-    };
+      return {
+          meetings: [],
+       id: '',
+          message: '',
+          isError: false,
+    }
+    },
+  created(){
+        this.getMeetings()
   },
-  methods: {
+
+    methods: {
+        getMeetings(){
+            axios.get('/api/meetings').then(response => {
+                for (let i =0; i <response.data.length;i++) {
+                   this.getParticipants(response.data[i])
+                }
+                this.meetings = response.data
+            })
+                .catch(error => this.failure(`Błąd przy pobieraniu spotkań. Szczegóły błędu: ${error.response.data}`));
+        },
+
+        getParticipants(meeting) {
+            meeting.participants=[]
+            axios.get('/api/meetings', {
+                params: {
+                    title: meeting.title,
+                    description: meeting.description
+                }
+            }).then(response => {
+                this.id = response.data[0].id
+                const url = "/api/meetings/" + this.id + "/participants"
+                axios.get(url)
+                    .then(response => {
+                        for (let i =0; i <response.data.length;i++) {
+                            meeting.participants.push(response.data[i].login)
+                        }
+                    })
+                    .catch(error => this.failure(`Błąd przy pobieraniu uczestników. Szczegóły błędu: ${error.response.data}`));
+            })
+                .catch(error => this.failure(`Błąd przy pobieraniu spotkania. Szczegóły błędu: ${error.response.data}`));
+        },
+
     addNewMeeting(meeting) {
-      this.meetings.push(meeting);
+        axios.post('/api/meetings', meeting)
+            .then(() => {
+                this.getMeetings()
+            })
+            .catch(error => this.failure(`Błąd przy dodawaniu spotkania. Szczegóły błędu: ${error.response.data}`));
     },
+
     addMeetingParticipant(meeting) {
-      meeting.participants.push(this.username);
+    axios.get('/api/meetings', {params: {title: meeting.title, description: meeting.description}}).then(response => {
+            this.id = response.data[0].id
+            const url = "/api/meetings/" + this.id + "/participants"
+            axios.post(url, {login: this.username})
+                .then(() => {
+                    this.getParticipants(meeting)
+                })
+                .catch(error => this.failure(`Błąd przy dodawaniu uczestnika. Szczegóły błędu: ${error.response.data}`));
+    }).catch(error => this.failure(`Błąd przy wyszukaniu spotkania. Szczegóły błędu: ${error.response.data}`));
     },
+
     removeMeetingParticipant(meeting) {
-      meeting.participants.splice(meeting.participants.indexOf(this.username), 1);
+        axios.get('/api/meetings', {params: {title: meeting.title, description: meeting.description}}).then(response => {
+            this.id = response.data[0].id
+            axios.delete('/api/meetings/' + this.id + '/participants/' + this.username).then(() => {
+                this.getMeetings()
+            })
+                .catch(error => this.failure(`Błąd przy usuwaniu uczestnika. Szczegóły błędu: ${error.response.data}`));
+        })
+            .catch(error => this.failure(`Błąd przy wyszukaniu spotkania. Szczegóły błędu: ${error.response.data}`));
+
     },
     deleteMeeting(meeting) {
-      this.meetings.splice(this.meetings.indexOf(meeting), 1);
+        axios.get('/api/meetings', {params: {title: meeting.title, description: meeting.description}}).then(response => {
+            this.id = response.data[0].id
+            axios.delete('/api/meetings/' + this.id).then(() => {
+                this.getMeetings()
+            })
+                .catch(error => this.failure(`Błąd przy usuwaniu spotkania. Szczegóły błędu: ${error.response.data}`));
+        })
+            .catch(error => this.failure(`Błąd przy wyszukaniu spotkania. Szczegóły błędu: ${error.response.data}`));
+
     },
+        failure(message) {
+            this.message = message;
+            this.isError = true;
+        },
+        clearMessage() {
+            this.message = undefined;
+        },
   }
 }
 </script>
